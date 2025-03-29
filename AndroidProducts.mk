e8c16523eeb59d078780c471c16e3ae8ac404654
fun guy <39643236+AM4Ryzen@users.noreply.github.com>
2025-03-29 00:11:00 -0500

    Adjust for building RR Q

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_xz2c.mk
+    $(LOCAL_DIR)/rr_xz2c.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_xz2c-user \
-    lineage_xz2c-userdebug \
-    lineage_xz2c-eng
+    rr_xz2c-user \
+    rr_xz2c-userdebug \
+    rr_xz2c-eng
